c0ec62cc8e952a2acd77139d37d0215688bcee2d
SDodson <siddodson@gmail.com>
2016-07-11 19:21:30 -1000

    Got HTML file to be generated

--- server.js
+++ server.js
@@ -14,38 +14,13 @@ const fs = require('fs');
 // }
 const ELEMENT_TEMPLATE = ``;
 
-function newHTML(queried) {
-/*  var htmlInfo;
-  htmlInfo = */
-  return `<!DOCTYPE html>
-  <html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <title>The Elements - ${queried.elementName}</title>
-    <link rel="stylesheet" href="/css/styles.css">
-  </head>
-  <body>
-    <h1>Helium</h1>
-    <h2>H</h2>
-    <h3>Atomic number ${queried.elementAtomicNumber}</h3>
-    <p>${queried.elementName} is a chemical element with symbol ${queried.elementSymbol} and atomic number ${queried.elementAtomicNumber}. ${queried.elementDescription} </p>
-    <p><a href="/">back</a></p>
-  </body>
-  </html>`;
-  // return htmlInfo;
-}
-
-
-
 function renderTemplate(template, locals){
 
 }
-
 function writeFile(filePath, fileContent){
   fs.writeFile(filePath, fileContent);
   // error handling
 }
-
 /*4040404*/
   function send404(res){
     fs.readFile('./public/404.html', function(error, data){
@@ -54,7 +29,6 @@ function writeFile(filePath, fileContent){
       res.end();
     });
   }
-
 function handleGET(req, res){
   // ^ Read file from REQ, send that file to client
 // handleGet
@@ -85,21 +59,41 @@ function handleGET(req, res){
 function handlePOST(req, res){
   var queried;
   req.on('data', function(chunk) {
-    // console.log(chunk);
-    // console.log(chunk.toString());
-    req.on('end', function() {
-      queried = querystring.parse(chunk.toString());
-      console.log(queried);
-
-      fs.writeFile('./public/'+queried.elementName+".html", newHTML(queried), 'utf8', (err) => {
-      console.log('saved!');
-
-      });
-
-      // body = Buffer.concat(body).toString();
-      // at this point, `body` has the entire request body stored in it as a string
-    });
+  queried = querystring.parse(chunk.toString());
   });
+
+  req.on('end', function(chunk) {
+    console.log(queried);
+
+         /* ERROR STUFF
+          try { if(queried.elementName[length]===0);
+            //if(queried.elementName===undefined||queried.elementSymbol===undefined||queried.elementAtomicNumber===undefined||queried.elementDescription===undefined);
+           }
+          catch(err){
+            throw "Please fill in all boxes";
+            }*/
+    fs.writeFile('./public/'+(queried.elementName.toLowerCase())+".html", newHTML(queried), 'utf8', (err) => {
+    });
+          // body = Buffer.concat(body).toString();
+          // at this point, `body` has the entire request body stored in it as a string
+
+    indexHTMLChanger(req, res, queried);
+  });
+}
+
+
+function indexHTMLChanger(req, res, passObject){
+      console.log("we made it");
+
+    fs.readFile('./public/index.html', function(error, data) {
+      var stringed = data.toString();
+      var replaced = stringed.replace("elements","ELEPAHANTS");
+      console.log(replaced);
+      // res.writeHead(200, {'Content-Type': 'text/html'});
+      // res.write(data.toString());
+      res.end();
+    });
+
 }
 
 const server = http.createServer((req, res) => {
@@ -115,4 +109,27 @@ const server = http.createServer((req, res) => {
       send404(res);
   }
 });
+
+
+function newHTML(queried) {
+/*  var htmlInfo;
+  htmlInfo = */
+  return `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>The Elements - ${queried.elementName}</title>
+    <link rel="stylesheet" href="/css/styles.css">
+  </head>
+  <body>
+    <h1>${queried.elementName}</h1>
+    <h2>${queried.elementSymbol}</h2>
+    <h3>Atomic number ${queried.elementAtomicNumber}</h3>
+    <p>${queried.elementName} is a chemical element with symbol ${queried.elementSymbol} and atomic number ${queried.elementAtomicNumber}. ${queried.elementDescription} </p>
+    <p><a href="/">back</a></p>
+  </body>
+  </html>`;
+  // return htmlInfo;
+}
+
 server.listen(PORT, () => console.log (`server listening on port ${PORT}`));
