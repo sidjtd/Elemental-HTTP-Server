ed29ea594415c554d8bbf830e53f21720aaf2e3c
SDodson <siddodson@gmail.com>
2016-07-12 14:10:21 -1000

    Refactored PUT and Server code

--- server.js
+++ server.js
@@ -21,6 +21,11 @@ const fs = require('fs');
     // if( process.env.PORT ){
     //   PORT = process.env.PORT;
     // }
+
+  String.prototype.capitalizeFirstLetter = function() {
+  return this.charAt(0).toUpperCase() + this.slice(1);
+};
+
 const ELEMENT_TEMPLATE = ``;
 
 function renderTemplate(template, locals){
@@ -28,17 +33,31 @@ function renderTemplate(template, locals){
 //  __    __ _____  _____ __ __    _____  _____  __ __  __ _____
 //  \\ /\ //((   )) ||_// ||<<     ||_// ((   )) || ||\\||  ||
 //   \V/\V/  \\_//  || \\ || \\    ||     \\_//  || || \||  ||
-function handlePUT(){
+
+function handlePUT(req, res){
+
+  fs.readFile('./public/${queried.elementName}.html', function(error, data){
+    console.log(req.body.elementName);
+
+    if (error) {
+      return handlePOST(req, res);
+    }
+    // res.writeHead(404, {'Content-Type': 'text/html'});
+    // res.write(data.toString());
+    // res.end();
+
+    console.log("Your file already exists!");
+  });
 };
 
 function handleDELETE(req, res){
-    req.on('data', function(chunk) {
-      var queried = querystring.parse(chunk.toString());
-      fs.unlink(`./public/${queried.elementName}.html`);
-    //queried = querystring.parse(chunk.toString());
-    });
-}
+  var storage = '';
+  console.log("Handle Delete happened!");
+  console.log(req.url);
 
+    // var queried = querystring.parse(chunk.toString());
+    // fs.unlink(`./public/${queried.elementName}.html`);
+}
 /*  ----------------------------------------------------------------------------
    / ★ ★ ★ ★ ★ ★ ★ ★ ★ ★ ★ ★ ★   COMPLETED!  ★ ★ ★ ★ ★ ★ ★ ★ ★ ★ ★ ★ ★ ★ ★ ★ ★  \
    ______________________________________________________________________________
@@ -47,15 +66,15 @@ function handleDELETE(req, res){
  ____________________________________________________
  ----------------------------------------------------  */
 function handleGET(req, res){
-    // If the URL is empty, make it INDEX.HTML
-    if(req.url==='/'){
-      // ^ make index.html default doc
-      req.url = '/index.html';
-    }
-                      // Data comes in bugger format v
+  // If the URL is empty, make it INDEX.HTML
+  if(req.url==='/'){
+    // ^ make index.html default doc
+    req.url = '/index.html';
+  }
+                    // Data comes in bugger format v
   fs.readFile('./public/' + req.url, function(error, data){
     if(error) {
-      send404();
+      send404(res);
     }else{
       res.writeHead(200, {'Content-Type': 'text/html'});
       res.write(data.toString());
@@ -63,42 +82,41 @@ function handleGET(req, res){
     }
   });//readFile ---<<<
 }// main ---<<<
+
+
+
+
 /*                Handle Post ✓
  ____________________________________________________
  ----------------------------------------------------  */
 function handlePOST(req, res){
-  var queried;
-  req.on('data', function(chunk) {
-  queried = querystring.parse(chunk.toString());
-  });
+//console.log("✓",queried.elementName.length);
+  //ERROR STUFF
+  try {
+    if(req.body.elementName.length <= 2 || req.body.elementSymbol.length !== 2 || req.body.elementAtomicNumber.length !== 2){
+      throw "info missing";
+    }
+  }catch(err){
+    throw "Empty Entry and/or Symbol name is not 2 characters";
+  }
 
-  req.on('end', function(chunk) {
-    //console.log("✓",queried.elementName.length);
-          //ERROR STUFF
-          try { if(
-            queried.elementName.length<=2||
-            queried.elementSymbol.length!==2//||
-            //queried.elementSymbol === 'string'
-            //||
-            //queried.elementAtomicNumber===isNan
-            ) throw "name missing";
-            //if(queried.elementName===undefined||queried.elementSymbol===undefined||queried.elementAtomicNumber===undefined||queried.elementDescription===undefined);
-          }
-          catch(err){
-            throw "Empty Entry and/or Symbol name is not 2 characters";
-            }
-    fs.writeFile('./public/'+(queried.elementName.toLowerCase())+".html", newHTML(queried), 'utf8', (err) => {
-    });
-    indexHTMLChanger(req, res, queried);
-  });//readFile ---<<<
+  fs.writeFile('./public/'+(req.body.elementName.toLowerCase())+".html", newHTML(req.body), 'utf8', (err) => {
+    if (err){
+      throw 'POST: Cannot write file';
+    }
+    indexHTMLChanger(req, res, req.body);
+  });
 } // main ---<<<
+
+
 /*               Function - HTML Post Updater ✓
  ____________________________________________________
  ----------------------------------------------------  */
 function indexHTMLChanger(req, res, passObject){
+  var firstLetterCapitalized = passObject.elementName.capitalizeFirstLetter();
   var replacerText = `<li>
       <a href="/${passObject.elementName}.html">\
-      ${passObject.elementName}</a>\
+      ${firstLetterCapitalized}</a>\
     </li>
   </ol>`;
 
@@ -151,20 +169,35 @@ function newHTML(queried) {
  ____________________________________________________
  ----------------------------------------------------  */
 const server = http.createServer((req, res) => {
-  // someone connected
-  switch( req.method ){
-    case 'GET':
-      handleGET(req, res);
-      break;
-    case 'POST':
-      handlePOST(req, res);
-      break;
-    case 'DELETE':
-      handleDELETE(req, res);
-      break;
-    default:
-      send404(res);
-  }
+  var queried = '';
+
+  req.on('data', (chunk) => {
+    queried = querystring.parse(chunk.toString());
+  });
+
+  req.on('end', () => {
+    req.body = queried;
+    // someone connected
+    console.log("this is req.method",req.method);
+    switch( req.method ){
+      case 'GET':
+        handleGET(req, res);
+        break;
+      case 'POST':
+        handlePOST(req, res);
+        break;
+      case 'DELETE':
+        handleDELETE(req, res);
+        break;
+      case 'PUT':
+        handlePUT(req, res);
+        break;
+      default:
+        console.log("req",req,"res",res);
+        send404(res);
+    }
+  });
+
 });// main ---<<<
 /*______________________________________________________________________________
   ------------------------------------------------------------------------------  */
